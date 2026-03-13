8e8ce09a2bb351ce504eb4ace07dfb6a21872a6a
SnailShepherd <dm.3635287@gmail.com>
2026-03-13 01:13:57 +0100

    Update GameUi.kt

--- app/src/main/java/com/mordin/samathascope/GameUi.kt
+++ app/src/main/java/com/mordin/samathascope/GameUi.kt
@@ -1,5 +1,6 @@
 package com.mordin.samathascope
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -13,7 +14,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.BorderStroke
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
